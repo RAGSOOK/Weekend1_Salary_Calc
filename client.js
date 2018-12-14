9bddc9aad62ba4c4e1e339af7206e7721aca119f
Cody Troop <codytroop@yahoo.com>
2018-12-14 15:57:34 -0600

    Fields append to table on commit

--- client.js
+++ client.js
@@ -6,13 +6,24 @@
     //listens for submit
     $('#submit').on('click', function(){
     
+        //save values
         let first = $('#firstIn').val();
         let last = $('#lastIn').val();
         let ID = $('#idIn').val();
         let title = $('#titleIn').val();
         let annSal = $('#annSalIn').val();
 
-        console.log(first, last, ID, title, annSal);
+        //empty fields
+        $('#firstIn').empty();
+        $('#lastIn').empty();
+        $('#idtIn').empty();
+        $('#titleIn').empty();
+        $('#annSalIn').empty();
+
+        //add row to table
+        $('#tableBody').append('<tr><td>' +first+ '</td><td>' +last+ '</td><td>'+ID+'</td><td>'+title+'</td><td>'+annSal+'</td></tr>');
+
+        // console.log(first, last, ID, title, annSal);
     })
  });//end submit
  
