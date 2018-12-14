6fe7b4e9a2022f4d1fa03e90e70e683322cab09e
Cody Troop <codytroop@yahoo.com>
2018-12-14 15:51:51 -0600

    Button reacts and input fields successfully taken

--- client.js
+++ client.js
@@ -1,6 +1,26 @@
-      // Display each of the pets on the DOM.
-  for (let i = 0; i < pets.length; i++) {
-    const element = pets[i];
-    $('#tableBody').append('<tr> <td>' + element.name + '</td> <td>' + element.type + '</td> <td>' + element.age +'</td>  </tr>');
+ 
+ 
+//Jquery
+ $(document).ready(function(){
+
+    //listens for submit
+    $('#submit').on('click', function(){
+    
+        let first = $('#firstIn').val();
+        let last = $('#lastIn').val();
+        let ID = $('#idIn').val();
+        let title = $('#titleIn').val();
+        let annSal = $('#annSalIn').val();
+
+        console.log(first, last, ID, title, annSal);
+    })
+ });//end submit
+ 
+ 
+ 
+ // Display each of the pets on the DOM.
+//   for (let i = 0; i < pets.length; i++) {
+//     const element = pets[i];
+//     $('#tableBody').append('<tr> <td>' + element.name + '</td> <td>' + element.type + '</td> <td>' + element.age +'</td>  </tr>');
   
-  }
+//   }
