e97e6cefaf78fcbd22e7deb6500a74c7ddf67c7d
Cody Troop <codytroop@yahoo.com>
2018-12-15 12:07:27 -0600

    stretch goals - styling complete

--- client.js
+++ client.js
@@ -1,5 +1,5 @@
 class Employee{
-    constructor(firstIn, lastIn, idIN, titleIn, annSalIn ){
+    constructor(firstIn, lastIn, idIn, titleIn, annSalIn ){
         this.firstName = firstIn;
         this.lastName = lastIn;
         this.ID = idIn;
@@ -34,9 +34,9 @@ let roster = [];
         $('#annSalIn').val('');
 
         //add row to table
-        $('#tableBody').append('<tr><td><button class="removeButton">X</button></td><td>' +first+ '</td><td>' +last+ '</td><td>'+ID+'</td><td>'+title+'</td><td>'+annSal+'</td></tr>');
+        $('#tableBody').append('<tr><td><button class="removeButton">X</button></td><td>' +first+ '</td><td>' +last+ '</td><td class ="empID">'+ID+'</td><td>'+title+'</td><td>'+annSal+'</td></tr>');
 
-        updateMonthlyTotal(annSal);
+        addToMonthlyTotal(annSal);
 
         //Update Total Monthly on DOM
         $('#totalMonthlyExpenditure').html(monthlyTotal);
@@ -53,20 +53,51 @@ let roster = [];
  });
 
 // adds the monthly salary from an annual salary to total expenses
- let updateMonthlyTotal = function(indvAnnSal){
+ let addToMonthlyTotal = function(indvAnnSal){
      indvMonthSal = Number(indvAnnSal) / 12;
      monthlyTotal += indvMonthSal;
  }
 
+ // subtracts the monthly salary from an annual salary to total expenses
+ let subtractFromMonthlyTotal = function(indvAnnSal){
+     indvMonthSal = Number(indvAnnSal) / 12;
+     monthlyTotal -= indvMonthSal;
+ }
+
  //Deletes this row of the table
  let killRow = function(){
+
+    const delID = $(this).parent().siblings('.empID').text();
+    console.log('delID: '+delID);
+
+    let indexDel = -1;
+
+    for(let i = 0; i < roster.length; i++){
+        // console.log(roster[i].ID);
+        if (roster[i].ID == delID) {
+            indexDel = i;
+            break;
+        }
+    }
+
+    console.log('employee to remove ' + roster[indexDel].firstName);
+    subtractFromMonthlyTotal(roster[indexDel].annSal);
+    roster.splice(indexDel,1);
+
+    //Update Total Monthly on DOM
+    $('#totalMonthlyExpenditure').html(monthlyTotal);
+    if( monthlyTotal < 20000){
+       $('#totalMonthlyExpenditure').css('background-color', 'white');
+    }
+
      $(this).parent().parent().remove();
      console.log('kill row');
- }
+ }// end killRow
 
  // Adds new employee to roster
  function newEmployee( first, last, ID, title, annSal ){
     console.log( 'in newEmployee:',first, last, ID, title, annSal );
     roster.push( new Employee( first, last, ID, title, annSal ) );
+    // console.log(roster);
     return true;
   } // end newEmployee
